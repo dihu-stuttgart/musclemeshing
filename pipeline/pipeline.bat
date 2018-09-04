0dc8334a24655f0065a35adf1e464fe3833591a3
Tobias <st143615@stud.uni-stuttgart.de>
2018-09-04 16:30:33 +0200

    adjusted fibre thickness script and tried it on Biceps Streamlines

--- pipeline/pipeline.bat
+++ pipeline/pipeline.bat
@@ -1,4 +1,4 @@
-python ioDetect.py biceps.msh 10 10 2
+python ioDetect.py biceps.msh 5 5 2
 C:\Users\Tobias\Desktop\ProjektINF\onelab-Windows64\gmsh C:\Users\Tobias\Desktop\ProjektINF\Git-Repo\pipeline\muscle.pro -run
 python SLConvert.py muscleStreamline.pos
 C:\Users\Tobias\Desktop\ProjektINF\onelab-Windows64\gmsh C:\Users\Tobias\Desktop\ProjektINF\Git-Repo\pipeline\streamlines.geo
