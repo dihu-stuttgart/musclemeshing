597b79b48dd2854183a70de4f94f3a56c28407e1
Dobby1995 <t.s.walter@web.de>
2018-11-12 17:13:45 +0100

    Update pipeline.bat

--- pipeline/pipeline.bat
+++ pipeline/pipeline.bat
@@ -1,5 +1,5 @@
 python ioDetect.py biceps.msh 7 7 2
-C:\Users\Tobias\Desktop\ProjektINF\onelab-Windows64\gmsh C:\Users\Tobias\Desktop\ProjektINF\Git-Repo\pipeline\muscle.pro -run
+gmsh muscle.pro -run
 python SLConvert.py muscleStreamline.pos
-C:\Users\Tobias\Desktop\ProjektINF\onelab-Windows64\gmsh C:\Users\Tobias\Desktop\ProjektINF\Git-Repo\pipeline\streamlines.geo
+gmsh streamlines.geo
 pause
